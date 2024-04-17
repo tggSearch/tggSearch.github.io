52a9f162d4ee3d4e8cd0e0fff8384df931133c70
tggSearch <huangdh4@outlook.com>
2024-04-17 09:37:45 +0800

    add-okx-exchange

---
+++ submitToBing.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+async function postIndexNow(key, url) {
+    const requestData = {
+        host: "www.example.org",
+        key: key,
+        keyLocation: `https://tggsearch.org/${key}.txt`,
+        urlList: urls
+    };
+
+    try {
+        const response = await axios.post('https://api.indexnow.org/IndexNow', requestData, {
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                'Host': 'api.indexnow.org'
+            }
+        });
+
+        console.log('Response:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error posting to IndexNow:', error);
+        throw error;
+    }
+}
+
+// Example usage:
+const key = "93322823db424d08b5e9a49dfa428112";
+const url = [
+    "https://tggsearch.org/docs/telegram-no-sms-code.html",
+    "https://tggsearch.org/"
+];
+
+postIndexNow(key, url).then();
