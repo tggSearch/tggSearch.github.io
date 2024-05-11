46af7ba965dc240df51dad0e3633d68b84f23ee6
tggSearch <huangdh4@outlook.com>
2024-05-11 14:37:41 +0800

    update

--- change.js
+++ change.js
@@ -10,16 +10,17 @@ async function processFilesInDirectory(directoryPath) {
         for (const file of files) {
             const filePath = path.join(directoryPath, file);
 
-            // if(filePath !== '_posts/2023-11-15-telegram-driver-bus.md'){
-            //     continue;
-            // }
             // 读取文件内容
             let content = fs.readFileSync(filePath, 'utf8');
 
+            if(content.includes('this.onerror')) {
+                continue;
+            }
+
             // 使用正则表达式进行替换
             content = content.replace(
-                /<img\s*([^>]*)src="https:\/\/cdn\.jsdelivr\.net\/gh\/[^\/]+\/[^\/]+\/assets\/img\/([^"]+)"([^>]*)\/>/g,
-                '<img $1src="https://cdn.jsdelivr.net/gh/tggsearch/tggsearch.github.io/assets/img/$2"$3 onerror="this.onerror=null;this.src=\'/assets/img/$2\'" />'
+                /<img\s*([^>]*)src="https:\/\/cdn\.jsdelivr\.net\/gh\/[^\/]+\/[^\/]+\/assets\/img\/([^"]+)"([^>]*)>/g,
+                '<img $1src="https://cdn.jsdelivr.net/gh/tggsearch/tggsearch.github.io/assets/img/$2"$3 onerror="this.onerror=null;this.src=\'/assets/img/$2\'">'
             );
 
             // 写入替换后的内容到文件中
@@ -34,7 +35,7 @@ async function processFilesInDirectory(directoryPath) {
 }
 
 // 指定要处理的目录
-const directoryPath = './';
+const directoryPath = './_posts';
 
 // 调用函数处理目录下的所有文件
 processFilesInDirectory(directoryPath);
