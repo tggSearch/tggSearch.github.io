9ac6954077ac393239b250e350d793696709d6fa
tggSearch <huangdh4@outlook.com>
2024-11-25 14:15:30 +0800

    a

---
+++ assets/js/ip-check.js
@@ -0,0 +1,27 @@
+// 设置链接的函数
+function setLinks(countryCode) {
+  const registerButtons = document.getElementsByClassName('register-button');
+
+  const linkHref = countryCode === 'CN' 
+    ? './302.html?target=https://www.mnftinqq.com/join/90884854' 
+    : './302.html?target=https://www.okx.com/join/90884854';
+
+  for (let button of registerButtons) {
+    const anchorTags = button.querySelectorAll('a'); // 查找所有<a>标签
+    anchorTags.forEach(anchorTag => {
+      anchorTag.href = linkHref; // 统一设置按钮的链接
+    });
+  }
+}
+
+// 使用免费的 IP 查询服务
+fetch('https://api.ip.sb/geoip')
+  .then(response => response.json())
+  .then(data => {
+    setLinks(data.country_code); // 直接传入国家代码
+  })
+  .catch(error => {
+    console.error('无法获取用户 IP 信息', error);
+    // 默认使用中国的链接
+    setLinks('CN'); // 设置默认链接为中国
+  }); 
