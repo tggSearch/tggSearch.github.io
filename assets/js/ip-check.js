52530e034d632f906404f4435e624fb9cb025f6a
huangdanhua <9402020+huangdh3@user.noreply.gitee.com>
2025-06-13 10:52:01 +0800

    default-tggsearch

--- assets/js/ip-check.js
+++ assets/js/ip-check.js
@@ -4,7 +4,7 @@ function setLinks(countryCode) {
   const downloadButtons = document.getElementsByClassName('download-button');
 
   const linkHref = countryCode === 'CN' 
-    ? '/docs/302.html?target=https://www.ouxyi.link/ul/6CngT5?channelId=90884854' 
+    ? '/docs/302.html?target=https://www.ouyizh.net/join/90884854' 
     : '/docs/302.html?target=https://www.okx.com/join/90884854';
 
   const downloadLinkHref = countryCode === 'CN' 
