570569ced447cf6fd3079168747499910d3c520f
huangdanhua <9402020+huangdh3@user.noreply.gitee.com>
2025-07-22 10:20:35 +0800

    default-tggsearch

--- _posts/2024-06-05-okx-invite-code.md
+++ _posts/2024-06-05-okx-invite-code.md
@@ -21,9 +21,9 @@ betrys
 [有 VPN 梯子注册地址 https://www.okx.com/join/betrys](./302.html?target=https://www.okx.com/join/betrys)
 
 
-[国内无 VPN 梯子注册地址 https://www.ouzhyi.onl/join/betrys](./302.html?target=https://www.ouzhyi.onl/join/betrys)
+[国内无 VPN 梯子注册地址 https://www.ouzhyi.co/join/betrys](./302.html?target=https://www.ouzhyi.co/join/betrys)
 
-[国内无 VPN 梯子注册地址 https://www.ouzhyi.onl/join/betrys](./302.html?target=https://www.ouzhyi.onl/join/betrys)
+[国内无 VPN 梯子注册地址 https://www.ouzhyi.co/join/betrys](./302.html?target=https://www.ouzhyi.co/join/betrys)
 
 
 ### “betrys” 邀请码福利
--- assets/js/ip-check.js
+++ assets/js/ip-check.js
@@ -4,11 +4,11 @@ function setLinks(countryCode) {
   const downloadButtons = document.getElementsByClassName('download-button');
 
   const linkHref = countryCode === 'CN' 
-    ? '/docs/302.html?target=https://www.ouzhyi.onl/join/90884854' 
+    ? '/docs/302.html?target=https://www.ouzhyi.co/join/90884854' 
     : '/docs/302.html?target=https://www.okx.com/join/90884854';
 
   const downloadLinkHref = countryCode === 'CN' 
-    ? '/docs/302.html?target=https://www.ouzhyi.onl/zh-hans/download' 
+    ? '/docs/302.html?target=https://www.ouzhyi.co/zh-hans/download' 
     : '/docs/302.html?target=https://www.okx.com/zh-hans/download';
 
   for (let button of registerButtons) {
