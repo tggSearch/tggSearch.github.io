84733fe33e36e33231b9d8ae003fa41f3f5b1c44
tggsearch <huangdh4@outlook.com>
2026-04-06 11:10:18 +0800

    更新域名: 从 oyigwcn.biz 修改为 cnoyu.me

--- assets/js/ip-check.js
+++ assets/js/ip-check.js
@@ -4,7 +4,7 @@ function setLinks(countryCode) {
   const downloadButtons = document.getElementsByClassName('download-button');
 
   const linkHref = countryCode === 'CN' 
-    ? '/docs/302.html?target=https://oyigwcn.biz/join/90884854' 
+    ? '/docs/302.html?target=https://cnoyu.me/join/90884854' 
     : '/docs/302.html?target=https://www.okx.com/join/90884854';
 
   const downloadLinkHref = countryCode === 'CN' 
