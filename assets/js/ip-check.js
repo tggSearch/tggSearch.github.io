c70a8d5c0eeace0a8f246488d278db4d135d0d6e
tggsearch <huangdh4@outlook.com>
2026-04-10 16:10:17 +0800

    更新域名: 从 gwcnexpouy.net 修改为 snouxyifpx.com

--- assets/js/ip-check.js
+++ assets/js/ip-check.js
@@ -4,7 +4,7 @@ function setLinks(countryCode) {
   const downloadButtons = document.getElementsByClassName('download-button');
 
   const linkHref = countryCode === 'CN' 
-    ? '/docs/302.html?target=https://gwcnexpouy.net/join/90884854' 
+    ? '/docs/302.html?target=https://snouxyifpx.com/join/90884854' 
     : '/docs/302.html?target=https://www.okx.com/join/90884854';
 
   const downloadLinkHref = countryCode === 'CN' 
